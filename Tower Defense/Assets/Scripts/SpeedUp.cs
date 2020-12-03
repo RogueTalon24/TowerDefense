7cbc29ab9b133924db8386c78b76d997b65da6dc
Jes Papania <44932245+RogueTalon24@users.noreply.github.com>
2020-12-02 22:00:07 -0500

    Tower Defense
    apk has apk file for android
    capstone Project build ajd the built vesion of game so just run executable
    and Tower Defense folder has unity project

---
+++ Tower Defense/Assets/Scripts/SpeedUp.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/*
+ * Jesus Papania
+ * Code for making game speed faster or slower and change text of button
+ */
+public class SpeedUp : MonoBehaviour
+{
+    public bool fast = false;
+    private string speed = "";
+    private Text currentSpeed;
+
+    public void DoubleTime()
+    {
+        //if the current speed is fast then show speed down 
+        if (fast == false)
+        {
+            Time.timeScale = 2f;
+            fast = true;
+            speed = "Speed Down";
+        }
+        else //the speed is normal and button shows speed up
+        {
+            Time.timeScale = 1f;
+            fast = false;
+            speed = "Speed Up";
+        }
+        //sets button text to speed variable
+        GameObject speedText = GameObject.Find("CurrentSpeed");
+        currentSpeed = speedText.GetComponent<Text>();
+        currentSpeed.text = speed;
+    }
+
+}
---
+++ Tower Defense/Assets/Scripts/TowerDefense/UI/ExitButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace TowerDefense.UI
+{
+	/// <summary>
+	/// A button for exiting the game
+	/// </summary>
+	public class ExitButton : Button
+	{
+		/// <summary>
+		/// Close the game when this button is clicked
+		/// </summary>
+		public override void OnPointerClick(PointerEventData eventData)
+		{
+			Application.Quit();
+		}
+
+		/// <summary>
+		/// Disable this button on mobile platforms
+		/// </summary>
+		protected override void Awake()
+		{
+			base.Awake();
+
+			if (Application.isPlaying)
+			{
+				gameObject.SetActive(true);
+			}
+		}
+	}
+}

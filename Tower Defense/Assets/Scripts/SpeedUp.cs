fd7a0603cf935e81c15e53cda44b53458bc80fab
Jes Papania <44932245+RogueTalon24@users.noreply.github.com>
2020-12-17 18:59:50 -0500

    Fixes
    Fixed stars at end and speed button code

--- Tower Defense/Assets/Scripts/SpeedUp.cs
+++ Tower Defense/Assets/Scripts/SpeedUp.cs
@@ -1,5 +1,4 @@
-//Made by Jesus Papania
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -16,22 +15,26 @@ public class SpeedUp : MonoBehaviour
     public void DoubleTime()
     {
         //if the current speed is fast then show speed down 
-        if (fast == false)
-        {
-            Time.timeScale = 2f;
-            fast = true;
-            speed = "Speed Down";
-        }
-        else //the speed is normal and button shows speed up
-        {
-            Time.timeScale = 1f;
-            fast = false;
-            speed = "Speed Up";
-        }
+        //if (fast == false)
+        //{
+        //    Time.timeScale = 2f;
+        //    fast = true;
+        //    speed = "Speed Down";
+        //}
+        //else //the speed is normal and button shows speed up
+        //{
+        //    Time.timeScale = 1f;
+        //    fast = false;
+        //    speed = "Speed Up";
+        //}
+
+        Time.timeScale = fast ? 1f : 2f;
+        speed = fast ? "Speed Up":"Speed Down";
+        fast = !fast;
+
         //sets button text to speed variable
         GameObject speedText = GameObject.Find("CurrentSpeed");
         currentSpeed = speedText.GetComponent<Text>();
         currentSpeed.text = speed;
     }
-
 }
